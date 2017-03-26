1798452d7a4d3d2eac35c2a41931858e2e79f011
Tom <trough23@gmail.com>
2017-03-26 13:57:10 +0000

    added pages and changed headings

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   end
   resources:searches
   resources:stores
+  
+  get '/about', to: 'pages#about'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
